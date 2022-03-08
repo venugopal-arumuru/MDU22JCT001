3184e10460d1c037f64aa3e71e942c8e90c2f74e
venugopal <85633400+venugopal-arumuru@users.noreply.github.com>
2022-03-08 11:11:34 +0530

    Test1 file modified

--- DemoApp/src/main/java/com/demoapp/Test1.java
+++ DemoApp/src/main/java/com/demoapp/Test1.java
@@ -5,6 +5,8 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
+		int x = 100;
+		System.out.println("X value is " + x);
 	}
 
 }
