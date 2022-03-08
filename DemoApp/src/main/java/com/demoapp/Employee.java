b622f89e5984571ab7e935b0838c83548bd6ef1c
venugopal <85633400+venugopal-arumuru@users.noreply.github.com>
2022-03-08 11:06:55 +0530

    First Commit

---
+++ DemoApp/src/main/java/com/demoapp/Employee.java
@@ -0,0 +1,26 @@
+package com.demoapp;
+
+public class Employee {
+	private int eno;
+	private String ename;
+	private  int sal;
+
+	public int getEno() {
+		return eno;
+	}
+	public void setEno(int eno) {
+		this.eno = eno;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public int getSal() {
+		return sal;
+	}
+	public void setSal(int sal) {
+		this.sal = sal;
+	}
+}
